a289444cc720cf22a5feeb06df87e0d4f0867478
최진이 <jini.choi@choejin-iui-MacBookAir.local>
2023-06-13 12:24:34 +0900

    prettier적용

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import Choijini from './Choijini';
 // import Main from './pages/Main/Main';
-import Login from './pages/Login/Login';
-import './styles/common.css';
-import './styles/reset.css';
+import Login from "./pages/Login/Login";
+import "./styles/common.css";
+import "./styles/reset.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Login />
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,21 +1,29 @@
 import "./Login.css";
 
 const Login = () => {
-    return <>
-        <div class="loginBox">
-            <div class="logo"><h1>Westagram</h1></div>
-            <div class="loginInput">
-                <form>
-                    <input type="text" placeholder="전화번호, 사용자 이름 또는 이메일" id="id" />
-                    <input type="password" placeholder="비밀번호" id="pw" />
-                    <button id="loginBtn" onclick="location='./main.html'" disabled>로그인</button>
-                </form>
-            </div>
-            <span class="findPassword">
-                비밀번호를 잊으셨나요?
-            </span>
+  return (
+    <>
+      <div class="loginBox">
+        <div class="logo">
+          <h1>Westagram</h1>
         </div>
-    </>;
-}
+        <div class="loginInput">
+          <form>
+            <input
+              type="text"
+              placeholder="전화번호, 사용자 이름 또는 이메일"
+              id="id"
+            />
+            <input type="password" placeholder="비밀번호" id="pw" />
+            <button id="loginBtn" onclick="location='./main.html'" disabled>
+              로그인
+            </button>
+          </form>
+        </div>
+        <span class="findPassword">비밀번호를 잊으셨나요?</span>
+      </div>
+    </>
+  );
+};
 
 export default Login;
